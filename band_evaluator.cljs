ff1f6cc9afe7d9770b0ef81b4cea1597c89e05a0
Bryce Kerley <bkerley@brycekerley.net>
2015-12-09 20:30:28 -0500

    add cond and condp support

--- band_evaluator.cljs
+++ band_evaluator.cljs
@@ -13,10 +13,41 @@
     (println "🖕🏿🎅🏿your opinion of" band_name "is WRONG 🖕🏿🎅🏿")
     ))
 
+(defn cond_evaluator
+  "fart"
+  [band_name]
+  (let [quality (get
+                  band_quality
+                  (-> band_name
+                      .toLowerCase
+                      keyword))]
+  (cond
+    (= quality :great)
+    (println "you like" band_name "and have taste")
+    (= quality :okay)
+    (println "i agree," band_name "is okay!")
+    :else
+    (println "🖕🏿🎅🏿your opinion of" band_name "is WRONG 🖕🏿🎅🏿")
+    )))
+
+(defn condp_evaluator
+  "fart"
+  [band_name]
+  (condp = (get band_quality (-> band_name
+                              .toLowerCase
+                              keyword))
+    :great (println "you like" band_name "and have taste")
+    :okay (println "i agree," band_name "is okay!")
+    (println "🖕🏿🎅🏿your opinion of" band_name "is WRONG 🖕🏿🎅🏿")
+    ))
+
 (band_evaluator "spoon")
-(band_evaluator "radiohead")
-(band_evaluator "GWAR")
+(condp_evaluator "radiohead")
+(cond_evaluator "GWAR")
 
 (def band_quality (assoc band_quality :gwar :great))
 
 (band_evaluator "GWAR")
+
+(let [{spoon :spoon decemberists :decemberists} band_quality]
+  (println spoon decemberists))
