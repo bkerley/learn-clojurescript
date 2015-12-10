9df32f4b963ce9195aa8ba82e27aa5b50d8e4731
Bryce Kerley <bkerley@brycekerley.net>
2015-12-09 19:41:32 -0500

    smarter band evaluator

--- fart.cljs
+++ fart.cljs
@@ -3,4 +3,21 @@
   [band_name]
   (println "my favorite band is" band_name))
 
+(def band_quality {:spoon :great
+                   :radiohead :okay
+                   :black_keys :great})
+
+(defn band_evaluator
+  "fart"
+  [band_name]
+  (case (get band_quality (keyword band_name))
+    :great (println "you like" band_name "and have taste")
+    :okay (println "i agree," band_name "is okay!")
+    (println "🖕🏿🎅🏿your opinion of" band_name "is WRONG 🖕🏿🎅🏿")
+    ))
+
 (my_band "spoon")
+
+(band_evaluator "spoon")
+(band_evaluator "radiohead")
+(band_evaluator "TOOL")
